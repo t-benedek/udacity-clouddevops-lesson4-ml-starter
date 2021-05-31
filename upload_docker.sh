26e01098560d2ae8aa7ede4ef16cae310fe81e16
t.benedek@web.de <t.beneden@web.de>
2021-05-31 23:20:12 +0200

    container uploaded to docker hub successfully

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=tbenedek/ml-housing
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u tbenedek -p get6lost
 
 # Step 3:
-# Push image to a docker repository
+docker tag ml-housing tbenedek/ml-housing
+docker push $dockerpath
