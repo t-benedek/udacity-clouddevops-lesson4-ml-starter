57ab69df56ed1ddc3f6d6c91bda3246fd6e7ac0d
t.benedek@web.de <t.beneden@web.de>
2021-05-31 23:51:43 +0200

    deploying on kubernetes done

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
 # This tags and uploads an image to Docker Hub
+kubectl delete deployment ml-housing
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=tbenedek/ml-housing
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment ml-housing --image=$dockerpath --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/ml-housing 8000:8000
 
