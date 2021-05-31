24ae78df8d2a59c6113a374008b113bd5781a86e
t.benedek@web.de <t.beneden@web.de>
2021-05-31 22:36:17 +0200

    updated to port 8000

--- run_docker.sh
+++ run_docker.sh
@@ -3,10 +3,10 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+docker build --tag=ml-housing .
 
 # Step 2: 
-# List docker images
+docker images
 
 # Step 3: 
-# Run flask app
+docker run -it -p8000:8000 --rm ml-housing
